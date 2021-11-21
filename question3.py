8bbe78ae98389b0096df9e32b172c70442dce0e1
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-11-21 15:51:12 +0200

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,11 +1,10 @@
 def encryption(n):
     matrix = [[],[]]
     for i in range(0,len(n)):
-        print("I is" + str(i))
         if i%2 == 0:
             
             matrix[0].append(n[i])
-            print("The string is " + n[i])
+            
         else:
             
             matrix[1].append(n[i])
