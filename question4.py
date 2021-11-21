063e9a5e9ec97eebd5713e4bb085a1727e88c6f2
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-11-21 16:20:18 +0200

    Update question4.py

--- question4.py
+++ question4.py
@@ -3,11 +3,11 @@ def superdigit(n,k):
     loop_digit = new_digit
     
     while len(loop_digit) != 1:
-        print("Loop digit before " + loop_digit)
+        
         add = 0
         for i in range(0,len(loop_digit)):
             add+=int(loop_digit[i])
         loop_digit = str(add)
-        print("Loop digit after "+loop_digit)
+        
     return loop_digit
 print(superdigit('148',3))
