a0001213ea1ae38692cac0481bef0055ca5fe51e
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-12-15 02:04:21 +0200

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,13 +1,22 @@
 def superdigit(n,k):
-    new_digit = n*k
-    loop_digit = new_digit
+    # initializing new digit which is a product of the input digit and the number of times it is to be repeated
+    loop_digit = n*k
     
+    
+    # While loop that runs so long as the length of the loop_digit is not equal to zero
     while len(loop_digit) != 1:
         
-        add = 0
-        for i in range(0,len(loop_digit)):
-            add+=int(loop_digit[i])
-        loop_digit = str(add)
+        # initialzing variable that would be used to hold temporal sum values for every iteration
+        add = 0 
         
+        #initialize for loop that runs from zero to the length of the loop)digit
+        for i in range(0,len(loop_digit)):
+            
+            # incrementing our temporal sum variable with the value of the loop_digit with index location being the current for loop oeration and converting it to a string
+            add+=int(loop_digit[i])
+            #update loop_value with a string of ou temporal sum variable
+        loop_digit = str(add)
+      
+    #return loop_digit
     return loop_digit
 print(superdigit('148',3))
