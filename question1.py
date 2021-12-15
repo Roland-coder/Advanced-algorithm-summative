48bd5c4ce28b2afcb4ab1f78baf05f0850af4357
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-12-15 02:06:48 +0200

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,7 +1,14 @@
 def firstnsum(n):
+    #initialize counter variable
+  
     x = 0
+    
+    # loop through from 1 to n+1 since n is supposed to be inclusive in our sum
     for i in range(1,n+1):
+        # increment counter variable by loop operation number
         x+=i
+    
+    # Retrun the value of x
     return x
 print(firstnsum(100000))
 
